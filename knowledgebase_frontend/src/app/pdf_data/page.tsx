a2ae1b76408213982e9ae5ba4e4dc358c889c654
Asif Rahaman <asifrahaman162@gmail.com>
2024-04-04 09:59:34 +0530

    WIP: Updated the UI to display the available pdfs

--- knowledgebase_frontend/src/app/pdf_data/page.tsx
+++ knowledgebase_frontend/src/app/pdf_data/page.tsx
@@ -1,200 +1,172 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 import Link from "next/link";
-import PDFViewer from "./components/PdfPreview";
+import { Fragment,  } from "react";
+import {  Transition } from "@headlessui/react";
+import { Menu } from "@headlessui/react";
+import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
 
-import { Fragment, useRef, useState } from "react";
-import { Dialog, Transition } from "@headlessui/react";
-import { CheckIcon } from "@heroicons/react/24/outline";
+const statuses = {
+  Complete: "text-green-700 bg-green-50 ring-green-600/20",
+  "In progress": "text-gray-600 bg-gray-50 ring-gray-500/10",
+  Archived: "text-yellow-800 bg-yellow-50 ring-yellow-600/20",
+};
+const projects = [
+  {
+    id: 1,
+    name: "GraphQL API",
+    href: "#",
+    status: "Complete",
+    createdBy: "Leslie Alexander",
+    dueDate: "March 17, 2023",
+    dueDateTime: "2023-03-17T00:00Z",
+  },
+  {
+    id: 2,
+    name: "New benefits plan",
+    href: "#",
+    status: "In progress",
+    createdBy: "Leslie Alexander",
+    dueDate: "May 5, 2023",
+    dueDateTime: "2023-05-05T00:00Z",
+  },
+  {
+    id: 3,
+    name: "Onboarding emails",
+    href: "#",
+    status: "In progress",
+    createdBy: "Courtney Henry",
+    dueDate: "May 25, 2023",
+    dueDateTime: "2023-05-25T00:00Z",
+  },
+  {
+    id: 4,
+    name: "iOS app",
+    href: "#",
+    status: "In progress",
+    createdBy: "Leonard Krasner",
+    dueDate: "June 7, 2023",
+    dueDateTime: "2023-06-07T00:00Z",
+  },
+  {
+    id: 5,
+    name: "Marketing site redesign",
+    href: "#",
+    status: "Archived",
+    createdBy: "Courtney Henry",
+    dueDate: "June 10, 2023",
+    dueDateTime: "2023-06-10T00:00Z",
+  },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function Example() {
-  const items = ["Item 1", "Item 2", "Item 3"];
-
-  const [open, setOpen] = useState(false);
-
-  const cancelButtonRef = useRef(null);
-
   return (
     <>
-      <div className="h-screen w-screen overflow-y-hidden flex flex-col">
-        <nav className="bg-white border-gray-200 dark:bg-gray-800 dark:border-gray-800">
-          <div className="flex flex-row items-center  gap-2 p-4">
-            <Link href="/" className="flex items-center gap-2">
-              <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white font-sans">knowledgebase.ai - PDF DASHBOARD</span>
-            </Link>
-            <button
-              data-collapse-toggle="navbar-multi-level"
-              type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="navbar-multi-level"
-              aria-expanded="false"
+      <header className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+              viewBox="0 0 24 24"
             >
-              <span className="sr-only">Open main menu</span>
-              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-              </svg>
-            </button>
-            <div className="   ml-auto " id="navbar-multi-level">
-              <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-800 dark:border-gray-700">
-                <li>
-                  <button
-                    className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent  md:p-0 md:dark:bg-transparent"
-                    aria-current="page"
-                    onClick={(e) => {
-                      setOpen(true);
-                    }}
-                  >
-                    Upload New
-                  </button>
-                </li>
-
-                <li>
-                  <Link href="/" className="block py-2 px-3 text-white  rounded md:bg-transparent  md:p-0  md:dark:bg-transparent" aria-current="page">
-                    Home
-                  </Link>
-                </li>
-
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    Report issues
-                  </a>
-                </li>
-                <li>
-                  <Link
-                    href="/pricing"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    Pricing
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/contact"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    Contact
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-        <div className="flex flex-grow h-full">
-          <div className="w-1/6 flex flex-col items-center py-6 pb-20 px-4 gap-2 h-full overflow-y-scroll no-scrollbar">
-            {items.map((item, index) => (
-              <>
-                <button className="rounded-lg border  bg-white border-gray-200 p-4 w-full">
-                  <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                    <div className="flex flex-col justify-between p-4 leading-normal">
-                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    </div>
-                  </div>
-                </button>
-              </>
-            ))}
-          </div>
-          <div className="grow flex flex-row bg-gray-100 h-full">
-            {" "}
-            <div className="w-1/2 border-r border-gray-200 h-full  bg-red-200">
-              <PDFViewer filePath="https://powerunit-ju.com/wp-content/uploads/2021/04/Aurelien-Geron-Hands-On-Machine-Learning-with-Scikit-Learn-Keras-and-Tensorflow_-Concepts-Tools-and-Techniques-to-Build-Intelligent-Systems-OReilly-Media-2019.pdf" />
-            </div>
-            <div className="grow flex flex-col">
-              <div className="h-5/6">
-                <div className="flex items-start gap-2.5 w-full flex-col px-12 py-4 h-full overflow-y-scroll no-scrollbar">
-                  <div className="flex flex-col max-w-3/4 leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-200">
-                    <div className="flex items-center space-x-2 rtl:space-x-reverse">
-                      <span className="text-sm font-semibold text-gray-900 dark:text-black">Bonnie Green</span>
-                      <span className="text-sm font-normal text-gray-500 dark:text-gray-400">11:46</span>
-                    </div>
-                    <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-black">That is awesome. I think our users will really appreciate the improvements.</p>
-                    <span className="text-sm font-normal text-gray-500 dark:text-gray-400">Delivered</span>
-                  </div>
-
-                  <div className="flex flex-col ml-auto  max-w-3/4 leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
-                    <div className="flex items-center space-x-2 rtl:space-x-reverse">
-                      <span className="text-sm font-semibold text-gray-900 dark:text-white">Bonnie Green</span>
-                      <span className="text-sm font-normal text-gray-500 dark:text-gray-400">11:46</span>
-                    </div>
-                    <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">That is awesome. I think our users will really appreciate the improvements.</p>
-                    <span className="text-sm font-normal text-gray-500 dark:text-gray-400">Delivered</span>
-                  </div>
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span className="ml-3 text-xl font-sans font-semibold">Knowledgebase.ai</span>
+          </a>
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <a className="mr-5 hover:text-gray-900">First Link</a>
+            <a className="mr-5 hover:text-gray-900">Second Link</a>
+            <a className="mr-5 hover:text-gray-900">Third Link</a>
+            <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+          </nav>
+          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Button
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </div>
+      </header>
+      <div className="flex justify-center">
+        <ul role="list" className="divide-y w-5/6  divide-gray-100">
+          {projects.map((project) => (
+            <li key={project.id} className="flex items-center justify-between gap-x-6 py-5">
+              <div className="min-w-0">
+                <div className="flex items-start gap-x-3">
+                  <p className="text-sm font-semibold leading-6 text-gray-900">{project.name}</p>
+                  <p className={classNames(statuses[project.status as keyof typeof statuses], "rounded-md whitespace-nowrap mt-0.5 px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset")}>{project.status}</p>
+                </div>
+                <div className="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
+                  <p className="whitespace-nowrap">
+                    Due on <time dateTime={project.dueDateTime}>{project.dueDate}</time>
+                  </p>
+                  <svg viewBox="0 0 2 2" className="h-0.5 w-0.5 fill-current">
+                    <circle cx={1} cy={1} r={1} />
+                  </svg>
+                  <p className="truncate">Created by {project.createdBy}</p>
                 </div>
               </div>
-              <div className="w-full  grow    p-4 mb-16">
-                <textarea
-                  id="message"
-                  rows={2}
-                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Write your thoughts here..."
-                ></textarea>
+              <div className="flex flex-none items-center gap-x-4">
+                <Link
+                  href={"/pdf_data/123"}
+                  className="hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block"
+                >
+                  View project<span className="sr-only">, {project.name}</span>
+                </Link>
+                <Menu as="div" className="relative flex-none">
+                  <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
+                    <span className="sr-only">Open options</span>
+                    <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
+                  </Menu.Button>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a href="#" className={classNames(active ? "bg-gray-50" : "", "block px-3 py-1 text-sm leading-6 text-gray-900")}>
+                            Edit<span className="sr-only">, {project.name}</span>
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a href="#" className={classNames(active ? "bg-gray-50" : "", "block px-3 py-1 text-sm leading-6 text-gray-900")}>
+                            Move<span className="sr-only">, {project.name}</span>
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a href="#" className={classNames(active ? "bg-gray-50" : "", "block px-3 py-1 text-sm leading-6 text-gray-900")}>
+                            Delete<span className="sr-only">, {project.name}</span>
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
               </div>
-            </div>
-          </div>
-        </div>
+            </li>
+          ))}
+        </ul>
       </div>
-
-      <Transition.Root show={open} as={Fragment}>
-        <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
-          <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                enterTo="opacity-100 translate-y-0 sm:scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                  <div>
-                    <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-                      <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
-                    </div>
-                    <div className="mt-3 text-center sm:mt-5">
-                      <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                        Upload a new pdf
-                      </Dialog.Title>
-                      <div className="mt-2">
-                        <p className="text-sm text-gray-500">
-                          Upload any pdf from you local system or you can also paste any other url corresponding to the pdf.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
-                    <button
-                      type="button"
-                      className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
-                      onClick={() => setOpen(false)}
-                    >
-                      Deactivate
-                    </button>
-                    <button
-                      type="button"
-                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
-                      onClick={() => setOpen(false)}
-                      ref={cancelButtonRef}
-                    >
-                      Cancel
-                    </button>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition.Root>
     </>
   );
 }
